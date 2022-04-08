67d10602fd299861047c636a953cf849964cfae4
yi zhu <1047606598@qq.com>
2022-04-08 16:40:26 +0800

    添加基架

---
+++ SIP2Client.Tests/Sip2Client_Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIP2Client.Tests;
+public class Sip2Client_Test
+{
+    // TODO: 完成编写SIP2单元测试
+}
---
+++ SIP2Client/Sip2Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIP2Client;
+public abstract class Sip2Client
+{
+    // TODO: 完成编写SIP2基础功能实现和接口
+}
